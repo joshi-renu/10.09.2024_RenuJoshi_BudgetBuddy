95a70cb0ca961fec5d3617f1b5763a042a0210a7
Joshi <rejoshi@deloitte.com>
2024-09-13 18:39:45 +0530

    test

--- user/src/main/java/com/budgetbuddy/cloud/app/service/FinancialGoalService.java
+++ user/src/main/java/com/budgetbuddy/cloud/app/service/FinancialGoalService.java
@@ -88,6 +88,7 @@ public class FinancialGoalService {
         fg.setCurrAmount(fgReq.getCurrAmount());
         fg.setMonthYear(fgReq.getMonthYear());
         fg.setUser(user);
+
         return fg;
     }
 
